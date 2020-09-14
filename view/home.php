d78e769a029a2d3d6ce1c790949ffffa20db570d
Thomas <lefoux82@gmail.com>
2020-09-14 16:03:41 +0200

    Create menu/slider and input the <script> for animation slider

--- view/home.php
+++ view/home.php
@@ -1,7 +1,48 @@
-<?php $title = "Accueil"; ?>
+<?php $title = "Conception Web Thomas ORTA"; ?>
 
 <?php ob_start(); ?>
-<p>Hello</p>
+<nav id="menu">
+    <div id="thomasOrta">
+        <h1>Thomas ORTA</h1>
+        <strong>Freelance</stong>
+    </div>
+    <div id="menuNavigation">
+        <ul>
+            <li>
+                <i class="fas fa-home"></i> Accueil
+            </li>
+            <li>
+                Prestation
+            </li>
+            <li>
+                Porfolio
+            </li>
+            <li>
+                CV
+            </li>
+            <li>
+                Contact
+            </li>
+        </ul>
+    </div>
+</nav>
+<section id="slider">
+    <div class="slide">
+        <div class="slideTitle">
+            <h2>Developpeur Web</h2>
+        </div>
+        <img src="images/computerCodeOne" alt="ecran coder">
+    </div>
+    <div class="slide">
+        <div class="slideTitle">
+            <h2>Hébergement Web</h2>
+        </div>
+        <img src="images/computerCodeTwo" alt="ecran coder">
+    </div>
+</section>
+
+<script src="js/slider.js"></script>
+<script src="js/main.js"></script>
 
 <?php 
     $content = ob_get_clean(); 
