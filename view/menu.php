367ab358d926f9794d5469702e0779dc4ab01937
Thomas <lefoux82@gmail.com>
2020-09-15 11:02:05 +0200

    Create menu

---
+++ view/menu.php
@@ -0,0 +1,25 @@
+<nav id="menu">
+    <div id="thomasOrta">
+        <h1><a class="link" href="index.php?objet=home">Thomas ORTA</a></h1>
+        <strong>Freelance</stong>
+    </div>
+    <div id="menuNavigation">
+        <ul>
+            <li>
+                <a class="link" href="index.php?objet=home"><i class="fas fa-home"></i> Accueil</a>
+            </li>
+            <li>
+                <a class="link" href="#myServices">Mes services</a> 
+            </li>
+            <li>
+                Porfolio
+            </li>
+            <li>
+                CV
+            </li>
+            <li>
+                Contact
+            </li>
+        </ul>
+    </div>
+</nav>
