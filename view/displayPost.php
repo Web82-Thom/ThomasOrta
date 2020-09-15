e2df7c88a59f904788577cc5415704e61b516102
Thomas <lefoux82@gmail.com>
2020-09-16 01:14:40 +0200

    Add display all comments

--- view/displayPost.php
+++ view/displayPost.php
@@ -42,6 +42,35 @@
         </div>
     </form>
 </section>
+<section id="AllCommentDisplay">
+<h2 class='titleSection'> Tous les commentaires</h2>
+    <?php foreach ($comments as $comment) { ?>
+        <div id="commentDisplay">
+            <div id="commentAuthorContent">
+                <p id="commentAuthor">
+                    <strong><?= htmlspecialchars($comment->getAuthor()); ?></strong>
+                </p>
+                <p class="dateInfos">
+                    publié le <?= $comment->getCommentDate(); ?>
+                </p>
+            </div>
+            <p id="commentContent">
+                <?= htmlspecialchars($comment->getComment()); ?>
+            </p>
+            <div id="actionComment">
+                <p>
+                    <a id="report" class="adminLink" href="index.php?objet=post&amp;action=reportComment&id=<?= $comment->getId() ; ?>&postId=<?= $post->getId(); ?>" onclick="return(confirm('Etes-vous sûr de vouloir signaler ce commentaire ?')); window.location.reload(false)" ><?php if ($comment->getReport() == 1) { echo ''; } else { echo 'Signaler';} ?></a>
+                </p>
+                <p>
+                    <a class="adminLink" href="index.php?objet=post&amp;action=updateComment&id=<?= $comment->getId(); ?>&postId=<?= $comment->getPostId(); ?>"><?php if (isset($_SESSION['firstAdmin']) && $_SESSION['firstAdmin'] == 1) { echo 'Modifier'; } else { echo '';} ?></a>
+                </p>
+                <p id="reportSignal">
+                    <?php if ($comment->getReport() == 1) { echo 'Commentaire signalé'; } ?>
+                </p> 
+            </div>    
+        </div>
+    <?php } ?>
+</section>
 <?php 
     $content = ob_get_clean(); 
 
