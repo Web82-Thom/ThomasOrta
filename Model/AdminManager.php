e540592ec4c46c6b3b70214b7d6ca5646eb94b03
Thomas <lefoux82@gmail.com>
2020-09-17 09:22:09 +0200

    Create function hydrate and recovery admins

---
+++ Model/AdminManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Model;
+
+use Controller\Router;
+use PDO;
+
+class AdminManager extends Database 
+{
+    public function hydrate(array $data) 
+    {
+        $admin = new Admin();
+        foreach($data as $key => $value)
+        {
+            $method = 'set'.ucfirst($key);
+            if (method_exists($admin, $method)) {
+                $admin->$method($value);
+            }
+        }
+        
+        return $admin;
+    }
+    
+    // RECUPEARATION DES ADMINISTRATEURS
+    public function getAdmins()
+    {
+        $req = $this->getDataBase()->prepare('SELECT id, pseudo, email, pass, first_admin FROM admins');
+        $req->execute(array());
+        $admins = [];
+        while($data = $req->fetch(PDO::FETCH_ASSOC)) {
+            $admins[] = $this->hydrate($data);
+        }
+        $req->closeCursor();
+        
+        return $admins;
+    }
+}
