138be9807b533c5b4d2bcb2f4760a0b40f25829b
Thomas <lefoux82@gmail.com>
2020-09-17 13:33:00 +0200

    insert section CV and formContact

--- view/home.php
+++ view/home.php
@@ -180,6 +180,278 @@
         </figure>
     </div>
 </section>
+<section id="cVitae">
+    <h2>À propos de moi, mon Cv...</h2>
+    <div id="myCv">
+        <div id="infosMyCv">
+            <div id="myPicture">
+                <img src="images/identite.jpg" alt="Thomas ORTA">
+            </div>
+            <div id="banner"> 
+                <h2>Thomas ORTA</h2>
+                <p>
+                    Freelance / Developper Back-End et Front-End
+                </p>
+            </div>
+            <div id="myProfile">
+                <h4>PROFIL</h4>
+                <ul id="profile">
+                    <li>
+                        38 ans, marié, 1 enfant
+                    </li>
+                    <li>
+                        Adresse : 31510 route de Genebrières 82 230 LEOJAC
+                    </li>
+                    <li>
+                        Tel : 06 . 12 .14 .92 .55
+                    </li>
+                    <li>
+                        E-m@il : thom.orta@gmail.com	
+                    </li>
+                    <li>
+                        SiteWeb : <a href="index.php?objet=home">https://www.thomas-orta.com</a>
+                    <li>
+                        Véhicule et Permis B, C, EB, EC
+                    </li>
+                </ul>
+            </div>
+            <div id="myLog">
+                <h4>PROFIL</h4>
+                <ul id="log">
+                    <li>
+                        GitBash / GitHub
+                    </li>
+                    <li>
+                        Sublime Text
+                    </li>
+                    <li>
+                        Visual Studio Code
+                    </li>
+                    <li>
+                        FilleZilla	
+                    </li>
+                    <li>
+                        PhpMyAdmin
+                    <li>
+                        Wamp64
+                    </li>
+                    <li>
+                        Outils Web Developper
+                    </li>
+                    <li>
+                        Office
+                    </li>
+                </ul>
+            </div>
+            <div id="myLanguage">
+                <h4>Langues</h4>
+                <ul id="language">
+                    <li>
+                        Anglais : Moyen
+                    </li>
+                    <li>
+                        Français : Langue maternelle
+                    </li>
+                </ul>
+            </div>
+            <div id="myInterest">
+                <h4>Intérêts</h4>
+                <ul id="interest">
+                    <li>
+                        peche
+                    </li>
+                    <li>
+                        Nature
+                    </li>
+                    <li>
+                        Informatique
+                    </li>
+                    <li>
+                        Famille
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+        <div id=training>
+            <table class="tableTraining">
+                <caption>Formations</caption>
+                <tbody>
+                    <tr>
+                        <td>
+                            <p>1999 - 2001</p>
+                            <p>Montauban</p> 
+                        </td>
+                        <td>
+                            <p>Diplôme BEP Prod Méca Option Usinage</p>
+                            <p>Lycée Antoine Bourdelle</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2001 - 2003</p>
+                            <p>Toulouse</p> 
+                        </td>
+                        <td>
+                            <p>Diplôme BAC Prod Méca Option Usinage</p>
+                            <p>CFAI Midi-Pyrénné</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2003</p>
+                            <p>Toulouse</p> 
+                        </td>
+                        <td>
+                            <p>Diplôme BTS Prod Méca Option Usinage</p>
+                            <p>CFAI Midi-Pyrénné</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2006 - 2007</p>
+                            <p>Montauban</p> 
+                        </td>
+                        <td>
+                            <p>Diplôme Professionnel de Route FIMO-PL-SPL-CACES</p>
+                            <p>ECF</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2019 - 2020</p>
+                            <p>Léojac</p> 
+                        </td>
+                        <td>
+                            <p>Diplôme Developpeur Web Junior en cours d'aquisition</p>
+                            <p>OpenClassroom.com</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div id="experience">
+            <table class="tableExperience">
+                <caption>Expériences proféssionnelles</caption>
+                <tbody>
+                    <tr>
+                        <td>
+                            <p>1999 - 2003</p>
+                            <p>Cugnaux</p> 
+                        </td>
+                        <td>
+                            <p>Dedienne Industrie,</p>
+                            <p>Apprenti pour le BAC et le BTS</p>
+                            <p>Missons ou taches réalisées:</p>
+                            <ul>
+                                <li>Usinages conventionnel et numérique</li>
+                                <li>Ajustage</li>
+                                <li>Programation numérique</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2003 - 2004</p>
+                            <p>Midi-Pyrénées</p> 
+                        </td>
+                        <td>
+                            <p>Confort Général Européen,</p>
+                            <p>Commercial</p>
+                            <p>Missions ou tâches réalisées:</p>
+                            <ul>
+                                <li>prise de contact</li>
+                                <li>démonstration et vente d'affineur d'eau</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2004 - 2006</p>
+                            <p>Montauban</p> 
+                        </td>
+                        <td>
+                            <p>Farella</p>
+                            <p>Ouvrier qualifié</p>
+                            <p>Missions ou tâches réalisées:</p>
+                            <ul>
+                                <li>Réalisation de pièces usinées</li>
+                                <li>Contrôles</li>
+                                <li>Ajustages</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2007 - 20018</p>
+                            <p>Bressols</p> 
+                        </td>
+                        <td>
+                            <p>Bergo fruits / Satar</p>
+                            <p>Chauffeur SPL National</p>
+                            <p>Missions ou tâches réalisées:</p>
+                            <ul>
+                                <li>Chargement / Déchargement</li>
+                                <li>Conduite National</li>
+                                <li>Entretien du véhicule</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2018 - 2020</p>
+                            <p>Léojac</p> 
+                        </td>
+                        <td>
+                            <p>OpenClassroom</p>
+                            <p>Formation en ligne Développeur Web Junior</p>
+                            <p>Missions ou tâches réalisées:</p>
+                            <ul>
+                                <li>Réalisation de 5 projets</li>
+                                <li>Langages HTML, CSS, JavaScript, PHP, SQL, CMS</li>
+                                <li>Étude de marché / Business Model Canvas</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>2020</p>
+                            <p>Léojac</p> 
+                        </td>
+                        <td>
+                            <p>Freelance</p>
+                            <p>Développeur de site Web</p>
+                            <p>Missions ou tâches réalisées:</p>
+                            <ul>
+                                <li>Etude de votre projet</li>
+                                <li>Cahiers des charges</li>
+                                <li>Langages HTML, CSS, JavaScript, PHP, SQL, CMS</li>
+                                <li>site Vitrine</li>
+                                <li>site blog</li>
+                                <li>site sur mesure</li>
+                                <li>...tous les nouveaux projets</li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>    
+        </div>
+    </div>
+</section>
+<section id="formContact">
+    <h2 class="titleSection">Formulaire de contact</h2>
+    <form method="POST" action="index?objet=contact">
+        <h2>Contactez moi !</h2>
+        <div id="formContactInput">
+            <input id="inputContactName" type="text" name="name" placeholder="Nom" size="30"><br>
+            <input id="inputContactFirstName" type="text" name="firstName" placeholder="Prénom" size="30"><br>
+            <input id="inputContactEmail" type="email" name="email" placeholder="Votre Email" size="30" maxlength="50"><br>
+            <input id="inputContactTitle" type="text" name="title" placeholder="Titre" size="30"><br>
+            <textarea id="inputContactMessage" name="message" rows="10" cols="30" placeholder="Votre message" maxlength="150"></textarea><br>
+        </div>
+        <button id="formButton" type="submit" name="mailForm" title="Envoyer" >Envoyer votre message</button>
+    </form>
+</section>
 
 <script src="js/slider.js"></script>
 <script src="js/main.js"></script>
