21386468ed989d12ec14777781ff966cff79d1a0
Thomas <lefoux82@gmail.com>
2020-09-17 14:29:48 +0200

    create noAccess if not Admin

---
+++ view/noAccess.php
@@ -0,0 +1,13 @@
+<?php $title = "Page Administrateur"; ?>
+
+<?php ob_start(); ?>
+
+<div id="echoAccess">
+    <?php echo 'Seul les administrateurs peuvent accéder à la page Admin.'; ?>
+</div>
+    
+<?php
+$content = ob_get_clean(); 
+
+require_once('template.php');
+?>
