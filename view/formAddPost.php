2acf5cb0245f99cd87ed8b261ac86fd8081508fe
Thomas <lefoux82@gmail.com>
2020-09-17 14:37:31 +0200

    create view for add post

---
+++ view/formAddPost.php
@@ -0,0 +1,43 @@
+<?php $title = "Admin-Rédaction"; ?>
+
+<?php ob_start(); ?>
+
+<script>
+	tinymce.init({
+	selector: 'textarea',
+	plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+	toolbar_mode: 'floating',
+});
+</script>
+
+<div id ="form">
+	<form method="post" action="index.php?objet=post&action=add">
+		<h2>Rédaction de vos chapitres</h2>
+		<div class="labelTitle">
+			<h3><label for="title">Titre :</label></h3>
+			<input type="text" placeholder="Indiquer le Titre" name ="title" <?php if (isset($post)) {echo 'value="' . $post->getTitle(). '"';} ?>>
+		</div>
+		<div class="labelContent">
+			<h3><label for="content">Contenu :</label></h3>
+			<textarea id="mytextarea" placeholder ="Rédiger votre chapitre" name ="content"><?php if (isset($post)) {echo $post->getContent();} ?></textarea>
+		</div>
+        <div class='labelCategory'>
+            <h3><label for="category">Category</label></h3>
+            <select name="category" id="category">
+                <option value="">--Choisissez une catégorie--</option>
+                <option value="showcase">Site vitrine</option>
+                <option value="blog">Site Blog</option>
+                <option value="integrator">Integrateur</option>
+                <option value="wordpress">WordPress</option>
+            </select>
+
+        </div>
+			<button id="formButton" type="submit" value ="Envoyer un nouveau post!">Envoyer un nouveau post!</button>
+	</form>
+</div>
+
+<?php 
+	$content = ob_get_clean(); 
+
+	require_once('template.php');
+?>
