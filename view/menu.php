b8b412e9f6f1430a7e4ee24e9fb23b181a1c97ec
Thomas <lefoux82@gmail.com>
2020-09-17 09:11:50 +0200

    Add link and add sub menu for admin

--- view/menu.php
+++ view/menu.php
@@ -1,9 +1,9 @@
 <nav id="menu">
     <div id="thomasOrta">
         <h1><a class="link" href="index.php?objet=home">Thomas ORTA</a></h1>
-        <strong>Freelance</stong>
+        <strong>Freelance</strong>
     </div>
-    <div id="menuNavigation">
+    <nav id="menuNavigation">
         <ul>
             <li>
                 <a class="link" href="index.php?objet=home"><i class="fas fa-home"></i> Accueil</a>
@@ -12,7 +12,7 @@
                 <a class="link" href="index.php?#myServices">Mes services</a> 
             </li>
             <li>
-                Porfolio
+                <a class="link" href="index.php?#projects">Portfolio
             </li>
             <li>
                 CV
@@ -20,6 +20,17 @@
             <li>
                 Contact
             </li>
+            <li class="menuAdmin">
+                <a href="index.php?objet=admin&amp;action=login">Admin</a>
+                <ul class="sousMenu">
+                    <li>
+                        <a href="index.php?objet=admin&amp;action=destroy"><?php if (isset($_SESSION['firstAdmin'])) {echo 'Se déconnecter';} ?></a>
+                    </li>
+                    <li>
+                        <a href="index.php?objet=admin&amp;action=login"><?php if (!isset($_SESSION['firstAdmin'])) {echo 'Se connecter';}?></a>
+                    </li>
+                </ul>
+            </li>
         </ul>
-    </div>
+</nav>
 </nav>
