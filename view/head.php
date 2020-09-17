42c329bc4b145697fbaeabe5196f009096d77513
Thomas <lefoux82@gmail.com>
2020-09-17 14:42:50 +0200

    insert script for TinyMCE

--- view/head.php
+++ view/head.php
@@ -37,4 +37,5 @@
 	<!-- contrôler la façon dont les moteurs de recherches explorent et indexent une page -->
 	<meta name="robots" content="noindex" />
 	<script src="https://kit.fontawesome.com/67cbdf618e.js" crossorigin="anonymous"></script>
+	<script src="https://cdn.tiny.cloud/1/zlz60md847scmftjlvg9i0ekftezuwbh8yqp2v076ujm04oe/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
