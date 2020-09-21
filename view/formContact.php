a2432d85b53b4e2e3260a7f3b1e17bbc60def3d2
Thomas <lefoux82@gmail.com>
2020-09-21 09:24:38 +0200

    add div form and form-input, label

--- view/formContact.php
+++ view/formContact.php
@@ -2,19 +2,42 @@
 
 <?php ob_start(); ?>
 
-<section id="formContact">
-    <h2 class="titleSection">Formulaire de contact</h2>
-    <form method="POST" action="index?objet=contact">
-        <h2>Contactez moi !</h2>
-        <div id="formContactInput">
-            <input id="inputContactName" type="text" name="name" placeholder="Nom" size="30"><br>
-            <input id="inputContactFirstName" type="text" name="firstName" placeholder="Prénom" size="30"><br>
-            <input id="inputContactEmail" type="email" name="email" placeholder="Votre Email" size="30" maxlength="50"><br>
-            <input id="inputContactTitle" type="text" name="title" placeholder="Titre" size="30"><br>
-            <textarea id="inputContactMessage" name="message" rows="10" cols="30" placeholder="Votre message" maxlength="150"></textarea><br>
-        </div>
-        <button id="formButton" type="submit" name="mailForm" title="Envoyer" >Envoyer votre message</button>
-    </form>
+<section class="formContact">
+    <h2 class="titleFormContact">Formulaire de contact</h2>
+    <div class="form ">
+		<div class="hedding-title">
+			<h1><a href="index.php?objet=contact">Contactez moi !</a></h1>
+		</div>
+		<div class="hedding-description">
+            <p>
+                <a href="index.php?objet=contact">
+                    Ma mission est bien plus que le developpement de votre site internet, je suis là pour vous accompagner à la reussite de votre activitée. Nous étudirons ensemble votre projet! Contactez moi par ce formulaire ou par téléphone.
+                </a>
+            </p>
+            <p>
+                Thomas ORTA : 06.12.14.92.55
+            </p>
+		</div>
+		<div class="form-input">
+            <form method="POST" action="index?objet=contact">
+                <label for="name">Votre nom :</label><br>
+                <input type="text" placeholder="Entrez votre nom" name="name"><br>
+                
+                <label for="firstName">Votre Prenom :</label><br>
+				<input type="text" placeholder="Entrez votre prénom" name="firstName"><br>
+                
+                <label for="title">Titre de votre message :</label>
+                <input type="text" name="title" placeholder="Titre"><br>
+
+                <label for="email"> Votre Em@il :</label><br>
+				<input type="text" placeholder="Email..." name='email'></br>
+                
+                <label for="message">Votre message :</label>
+				<textarea rows="4" placeholder="Message.." name="message"></textarea>
+				<button class="button">Send Message</button>
+			</form> 
+		</div>
+	</div>
 </section>
 
 <script src="js/formContact.js"></script>
