7d7f0fd7797921cfb431e8f7c866e80803c5f311
Thomas <lefoux82@gmail.com>
2020-09-21 09:18:46 +0200

    Working on under-menu and semantic tag

--- view/menu.php
+++ view/menu.php
@@ -1,10 +1,10 @@
-<nav id="menu">
+<header>
     <div id="thomasOrta">
         <h1><a class="link" href="index.php?objet=home">Thomas ORTA</a></h1>
         <strong>Freelance</strong>
     </div>
-    <nav id="menuNavigation">
-        <ul>
+    <nav id="menu">
+        <ul id="menuNavigation">
             <li>
                 <a class="link" href="index.php?objet=home"><i class="fas fa-home"></i> Accueil</a>
             </li>
@@ -23,14 +23,14 @@
             <li class="menuAdmin">
                 <a href="index.php?objet=admin&amp;action=login">Admin</a>
                 <ul class="sousMenu">
-                    <li>
+                    <li class="sousMenuLi">
                         <a href="index.php?objet=admin&amp;action=destroy"><?php if (isset($_SESSION['firstAdmin'])) {echo 'Se déconnecter';} ?></a>
                     </li>
-                    <li>
+                    <li class="sousMenuLi">
                         <a href="index.php?objet=admin&amp;action=login"><?php if (!isset($_SESSION['firstAdmin'])) {echo 'Se connecter';}?></a>
                     </li>
                 </ul>
             </li>
         </ul>
-</nav>
-</nav>
+    </nav>
+</header>
