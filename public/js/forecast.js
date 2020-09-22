6ca77f8d64ecbc49013f17d36975192f312f51c4
Thomas <lefoux82@gmail.com>
2020-09-22 17:31:28 +0200

    modification url, call ajax, change verification(), add temps-min/max

--- public/js/forecast.js
+++ public/js/forecast.js
@@ -1,31 +1,31 @@
 class Forecast {
     constructor() {
-        this.url = "https://api.openweathermap.org/data/2.5/weather?q="+ localStorage.getItem('Ville') +"&lang=fr&units=metric&appid=7424467761a19cc2015304b9e49b5fad";
+        this.url = "https://api.openweathermap.org/data/2.5/weather?&lang=fr&units=metric&appid=7424467761a19cc2015304b9e49b5fad&q=";
+        this.cityDefault = "montauban";
+        
         this.eventListener();
-        this.initSetting();
+        this.verification();
     }
 
     eventListener() {
-        localStorage.removeItem('Ville');
         document.getElementById("button").addEventListener('click', () => this.verification());
-        
     }
 
     verification() {
         this.city = document.getElementById("cityValue").value;
-        this.requete();
-        if (this.city !== "") {
-            localStorage.setItem("Ville", this.city);
-        } else { 
-            alert("Veuillez renseigner une ville");
+        if (this.city === "") {
+            this.city = this.cityDefault;
         }
+
+        this.urlCity = this.url + this.city;
+        this.requete();
     }
 
     requete() {
         let self = this;
             // Requête XMLHttp JSON
             let requete = new XMLHttpRequest(); 
-            requete.open('GET', this.url); 
+            requete.open('GET', this.urlCity); 
             requete.send();
             requete.onload = function(e) {
                 if (requete.readyState === XMLHttpRequest.DONE) {
@@ -36,24 +36,18 @@ class Forecast {
                         document.getElementById("nameCity").textContent = 'Ville de ' + object.name + '.' ;
                         document.getElementById("description").textContent = 'Description : ' + object.weather[0].description + '.' ;
                         document.getElementById("temperature").textContent = 'Température : ' + Math.round(object.main.temp) + '°C.' ;
+                        document.getElementById("temperatureMax").textContent = 'Température-maxi : ' + Math.round(object.main.temp_max) + '°C.' ;
+                        document.getElementById("temperatureMin").textContent = 'Température-mini : ' + Math.round(object.main.temp_min) + '°C.' ;
+
                        
                         let image = object.weather[0].icon
                         this.icon = document.getElementById("icon").src = ('src', 'http://openweathermap.org/img/wn/'+ image + '.png');
                     } else if (requete.status === 404) { 
                         alert(" Veuillez vérifier l'ortographe' ou ville introuvable ");
-                        sessionStorage.removeItem('Ville');
-                    };
+                    }
                 } else {
                     alert('Un problème est intervenu, merci de revenir plus tard.');
                 }
             };
-    }
-
-    initSetting () {
-        window.addEventListener("DOMContentLoaded", () => {
-            if (localStorage.getItem('Ville')) {
-                localStorage.removeItem('Ville');
-            }
-        });
     }
 }
