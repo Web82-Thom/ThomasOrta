daa8bf9223af212885c26533e7a04035417331ee
Thomas <lefoux82@gmail.com>
2020-09-24 11:16:56 +0200

    add litle menu for responsive

--- view/menu.php
+++ view/menu.php
@@ -24,10 +24,49 @@
                 <a href="index.php?objet=admin&amp;action=login">Admin</a>
                 <ul class="sousMenu">
                     <li>
-                        <a href="index.php?objet=admin&amp;action=destroy"><?php if (isset($_SESSION['firstAdmin'])) {echo 'Se déconnecter';} ?></a>
+                        <a href="index.php?objet=admin&amp;action=destroy">
+                            <?php if (isset($_SESSION['firstAdmin'])) {echo 'Se déconnecter';} ?>
+                        </a>
                     </li>
                     <li>
-                        <a href="index.php?objet=admin&amp;action=login"><?php if (!isset($_SESSION['firstAdmin'])) {echo 'Se connecter';}?></a>
+                        <a href="index.php?objet=admin&amp;action=login">
+                            <?php if (!isset($_SESSION['firstAdmin'])) {echo 'Se connecter';}?>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <nav id="littleMenu">
+        <p><i class="fas fa-bars"></i> Menu</p> 
+        <ul id="menuNavigation">
+            <li class="listMenu">
+                <a class="link" href="index.php?objet=home"><i class="fas fa-home"></i> Accueil</a>
+            </li>
+            <li class="listMenu">
+                <a class="link" href="index.php?#myServices">Mes services</a> 
+            </li>
+            <li class="listMenu">
+                <a class="link" href="index.php?#projects">Portfolio</a>
+            </li >
+            <li class="listMenu">
+                <a class="link" href="index.php?#cVitae">CV</a>
+            </li>
+            <li class="listMenu">
+                <a class="link" href="index.php?#formContact">Contact</a> 
+            </li>
+            <li id="menuAdmin">
+                <a href="index.php?objet=admin&amp;action=login">Admin</a>
+                <ul class="sousMenu">
+                    <li>
+                        <a href="index.php?objet=admin&amp;action=destroy">
+                            <?php if (isset($_SESSION['firstAdmin'])) {echo 'Se déconnecter';} ?>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php?objet=admin&amp;action=login">
+                            <?php if (!isset($_SESSION['firstAdmin'])) {echo 'Se connecter';}?>
+                        </a>
                     </li>
                 </ul>
             </li>
