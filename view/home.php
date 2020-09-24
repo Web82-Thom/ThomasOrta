4a2e2f5e6ee99449146f5d626831391161521ee4
Thomas <lefoux82@gmail.com>
2020-09-24 11:17:57 +0200

    Add buton scroll, change and resive img, delete <hr>, change name class to camelCaze

--- view/home.php
+++ view/home.php
@@ -1,42 +1,44 @@
 <?php $title = "Conception Web Thomas ORTA"; ?>
 
 <?php ob_start(); ?>
-
+<div id="scrollToTop">
+    <a href="index.php"><i class="fas fa-arrow-circle-up"></i></a>
+</div>
 <section id="slider">
     <div class="slide">
         <div class="slideTitle">
             <h2>Developpeur Web</h2>
         </div>
-        <img src="images/computerCodeOne.jpg" alt="ecran coder">
-    </div>
-    <div class="slide">
-        <div class="slideTitle">
-            <h2>Hébergement Web</h2>
-        </div>
-        <img src="images/serveur.jpg" alt="ecran coder">
+        <img src="images/computerCodeOne.jpg" alt="ecran code">
     </div>
     <div class="slide">
         <div class="slideTitle">
             <h2>Objectifs et solutions</h2>
         </div>
-        <img src="images/objectif.jpg" alt="ecran coder">
+        <img src="images/objectifsSolutions.jpg" alt="srabble code">
+    </div>
+    <div class="slide">
+        <div class="slideTitle">
+            <h2>Hébergement Web</h2>
+        </div>
+        <img src="images/serveur.jpg" alt="SEO">
     </div>
 </section>
 <section id="forecast">
     <h2>Le petit coin météo...</h2>
     <div class="input">
-        <input for="button"type="text" id="cityValue" placeholder="Entrez votre ville">
+        <input type="text" id="cityValue" placeholder="Entrez votre ville">
         <input type="submit" value="Rechercher" id="button">
     </div>
     <div class="displayWeather">
         <div id="imgWeather">
             <img id="icon" src="" alt="">
         </div>
-        <h4 id="nameCity">Ville :</h1>
-        <p id="description">Description :</p>
-        <p id="temperature">Température :</p>
-        <p id="temperatureMax">Température-maxi :</p>
-        <p id="temperatureMin">Température-mini :</p>
+        <h4 id="nameCity"></h1>
+        <p id="description"></p>
+        <p id="temperature"></p>
+        <p id="temperatureMax"></p>
+        <p id="temperatureMin"></p>
     </div>
 </section>
 <section id="myServices">
@@ -71,7 +73,6 @@
                 </li>
             </ul>
         </div>
-        <hr>
         <div class="service">
             <h3><a class='link' href="index.php?objet=integrator">Integration Web</a></h3>
             <p>
@@ -101,7 +102,6 @@
                 </li>
             </ul>
         </div>
-        <hr>
         <div class="service">
             <h3><a class='link' href="index.php?objet=blog">Blog</a></h3>
             <p>
@@ -131,7 +131,6 @@
                 </li>
             </ul>
         </div>
-        <hr>
         <div class="service">
             <h3><a class='link' href="index.php?objet=wordPress">WordPress</a></h3>
             <p>
@@ -466,10 +465,10 @@
 <section id="formContact">
     <h2 class="titleFormContact">Formulaire de contact</h2>
     <div class="form ">
-		<div class="hedding-title">
+		<div class="heddingTitle">
 			<h1><a href="index.php?objet=contact">Contactez moi !</a></h1>
 		</div>
-		<div class="hedding-description">
+		<div class="heddingDescription">
             <p>
                 <a href="index.php?objet=contact">
                     Ma mission est bien plus que le developpement de votre site internet, je suis là pour vous accompagner à la reussite de votre activitée. Contactez moi par ce formulaire ou par téléphone.
@@ -479,23 +478,23 @@
                 Thomas ORTA : 06.12.14.92.55
             </p>
 		</div>
-		<div class="form-input">
-            <form method="POST" action="index?objet=contact">
+		<div class="formInput">
+            <form method="POST" action="index?objet=contact"><br>
                 <label for="name">Votre nom :</label><br>
-                <input type="text" placeholder="Entrez votre nom" name="name"><br>
+                <input type="text" placeholder="Entrez votre nom" name="name" /><br>
                 
                 <label for="firstName">Votre Prenom :</label><br>
-				<input type="text" placeholder="Entrez votre prénom" name="firstName"><br>
+				<input type="text" placeholder="Entrez votre prénom" name="firstName" /><br>
                 
-                <label for="title">Titre de votre message :</label>
+                <label for="title">Titre de votre message :</label><br>
                 <input type="text" name="title" placeholder="Titre"><br>
 
                 <label for="email"> Votre Em@il :</label><br>
-				<input type="text" placeholder="Email..." name='email'></br>
+				<input type="text" placeholder="Email..." name='email' /><br>
                 
                 <label for="message">Votre message :</label>
 				<textarea rows="4" placeholder="Message.." name="message"></textarea>
-				<button class="button">Send Message</button>
+				<button class="button">Envoyez votre message</button>
 			</form> 
 		</div>
 	</div>
