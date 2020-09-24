7e8898e74d34c1d4249748b1718cce94c131d722
Thomas <lefoux82@gmail.com>
2020-09-24 11:25:41 +0200

    Move div category in top

--- view/formAddPost.php
+++ view/formAddPost.php
@@ -13,6 +13,16 @@
 <div id ="form">
 	<form method="post" action="index.php?objet=post&action=add">
 		<h2>Rédaction de vos chapitres</h2>
+		<div class='labelCategory'>
+            <h3><label for="category">Catégorie</label></h3>
+            <select name="category" id="category">
+                <option value="">--Choisissez une catégorie--</option>
+                <option value="showcase">Site vitrine</option>
+                <option value="blog">Site Blog</option>
+                <option value="integrator">Integrateur</option>
+                <option value="wordpress">WordPress</option>
+            </select>
+        </div>
 		<div class="labelTitle">
 			<h3><label for="title">Titre :</label></h3>
 			<input type="text" placeholder="Indiquer le Titre" name ="title" <?php if (isset($post)) {echo 'value="' . $post->getTitle(). '"';} ?>>
@@ -21,17 +31,6 @@
 			<h3><label for="content">Contenu :</label></h3>
 			<textarea id="mytextarea" placeholder ="Rédiger votre chapitre" name ="content"><?php if (isset($post)) {echo $post->getContent();} ?></textarea>
 		</div>
-        <div class='labelCategory'>
-            <h3><label for="category">Category</label></h3>
-            <select name="category" id="category">
-                <option value="">--Choisissez une catégorie--</option>
-                <option value="showcase">Site vitrine</option>
-                <option value="blog">Site Blog</option>
-                <option value="integrator">Integrateur</option>
-                <option value="wordpress">WordPress</option>
-            </select>
-
-        </div>
 			<button id="formButton" type="submit" value ="Envoyer un nouveau post!">Envoyer un nouveau post!</button>
 	</form>
 </div>
