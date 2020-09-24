8497efcbada0d52a31ad639926bfb301857ea5a0
Thomas <lefoux82@gmail.com>
2020-09-24 11:20:15 +0200

    add link responsive for 820px, change POG and meta robots

--- view/head.php
+++ view/head.php
@@ -7,14 +7,12 @@
 	<meta http-equiv="refresh" content="3600" />
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<!-- @Media-Query -->
-<!--<link rel="stylesheet" media="screen and (max-width: 1024px)" href="css/1024resolution.css" type="text/css" />
-<link rel="stylesheet" media="screen and (max-width: 700px)" href="css/700resolution.css" type="text/css" />
-<link rel="stylesheet" media="screen and (max-width: 400px)" href="css/400resolution.css" type="text/css" />-->
+	<link rel="stylesheet" media="screen and (max-width: 820px)" href="css/820resolution.css" type="text/css" />
 	<!-- viewport ecran mobile -->
 	<!-- La largeur de mon site = largeur de l'appareil -->
 	<meta name="viewport" content="width=device-width initial-scale=1.0" />
 	<!-- affichage Icon-Apple -->
-<!--<link rel="icon" type="image/x-icon" href="jeanForteroche.ico" />
+<!--<link rel="icon" type="image/x-icon" href="thomasOrta.ico" />
 <link rel="apple-touch-icon" href="apple-touch-icon-114-precomposed.png" />
 <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-114-precomposed.png" /> -->
 	<!-- description de la page web -->
@@ -29,13 +27,13 @@
 	<meta name="copyright" content="Thomas ORTA" />
 	<!-- protocole Open Graph -->
 	<meta property="og:title" content="Conception Site Web" />
-	<meta property="og:type" content="Enterprise" />
+	<meta property="og:type" content="Freelance" />
 	<meta property="og:image" content="..." />
 	<meta property="og:url" content="thomas-dev.com" />
-	<meta property="og:description" content="Conception Site Web" />
+	<meta property="og:description" content="Développeur Site Web" />
 	<meta property="og:site_name" content="Thomas ORTA" />
 	<!-- contrôler la façon dont les moteurs de recherches explorent et indexent une page -->
-	<meta name="robots" content="noindex" />
+	<meta name="robots" content="ndex" />
 	<script src="https://kit.fontawesome.com/67cbdf618e.js" crossorigin="anonymous"></script>
 	<script src="https://cdn.tiny.cloud/1/zlz60md847scmftjlvg9i0ekftezuwbh8yqp2v076ujm04oe/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 	<script src="https://api.openweathermap.org/data/2.5/weather?q=France,fr&appid=7424467761a19cc2015304b9e49b5fad" referrerpolicy="origin"></script>
