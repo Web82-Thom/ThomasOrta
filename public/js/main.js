e5ba5de5e9a70a75c9d54f2a73d8fabe634fc294
Thomas <lefoux82@gmail.com>
2020-09-25 10:05:27 +0200

    call object FormContact.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 if (document.getElementById('slider')) {
     new Slider();
-
     new Forecast();
+    new FormContact();
+
+} else if (document.getElementsByClassName('formClass')) {
+    new FormContact();
 }
