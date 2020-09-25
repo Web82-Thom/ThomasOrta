8243e5105a6f9fee6996fc8ce75ac213b05769de
Thomas <lefoux82@gmail.com>
2020-09-26 01:01:33 +0200

    create footer.php

---
+++ view/footer.php
@@ -0,0 +1,60 @@
+<footer>
+    <p class="footerIntro">
+        <strong>
+            Une stratégie online n’est pas à négliger et notamment pour les secteurs d’activité qui enregistrent une forte demande sur internet.
+        </strong> 
+    </p>
+	<section id="contactInfos">
+        <h4 class="titleContact">Contact</h4>
+        <article id="contact">
+            <h4>Contact</h4>
+                <ul >
+                    <li>
+                        Thomas ORTA 06.12.14.92.55
+                    </li>
+                    <li>
+                        <a href="index.php?objet=contact">
+                            Formulaire de contact
+                        </a>
+                    </li>
+                    <li>
+                        Email: thom.orta@gmail.com
+                    </li>
+                    <li>
+                        Facebook (à venir..)
+                    </li>
+                    <li>
+                        SIRET: 888 067 550 00010
+                    </li>
+                    <li>
+                        APE: 6201Z
+                    </li>
+                </ul>
+        </article>
+        <article id="moreInfos">
+            <h4>Informations pratiques</h4>
+            <ul >
+                <li>
+                    <a href="index.php?objet=legalNotice">
+                        Mentions légales
+                    </a> 
+                </li>
+                <li>
+                    <a href="index.php?objet=cookie">
+                        Protection des données
+                    </a>
+                </li>
+            </ul>
+        </article>
+    </section>
+	<section id=infosFooter>
+		<p id="webCreator">
+            © 2020 Thomas ORTA
+        </p>
+		<p>
+			<a href="http://jigsaw.w3.org/css-validator/check/referer" >
+				<img style = "border: 0; width: 88px; height: 31px" src = "http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Valide !" />
+			</a>
+		</p>
+    </section>
+</footer>
