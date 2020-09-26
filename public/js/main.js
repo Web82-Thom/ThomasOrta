32054c936056154c4d012a5f580177d8b186e61c
Thomas <lefoux82@gmail.com>
2020-09-26 03:41:17 +0200

    insert object FormConnection and FormAddComment

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,10 @@ if (document.getElementById('slider')) {
     new Forecast();
     new FormContact();
 
-} else if (document.getElementsByClassName('formClass')) {
+} else if (document.getElementById('formConnection')) {
+    new FormConnection();
+} else if (document.getElementById('formAddComment')) {
+    new FormAddComment();
+} else if (document.getElementsByClassName('formContact')) {
     new FormContact();
 }
