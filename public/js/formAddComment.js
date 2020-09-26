f1e392a835230aef505f34f440cb071a888596a4
Thomas <lefoux82@gmail.com>
2020-09-26 03:44:50 +0200

    create objet formAddComment

---
+++ public/js/formAddComment.js
@@ -0,0 +1,23 @@
+class FormAddComment {
+    constructor() {
+        this.eventListenerMethod();
+
+        document.getElementById('author');
+        document.getElementById('comment');
+    }
+
+    eventListenerMethod() {
+        document.getElementById('submitComment').addEventListener('click' , () => this.verification());
+    }
+
+    verification() {
+        let author = document.getElementById('author').value;
+        let comment = document.getElementById('comment').value;
+
+        if (author !== "" && comment !== "" ) {
+            this.eventListenerMethod();
+        } else {
+            alert('Veuillez remplir tous les champs correctement.');
+        }
+    }
+}
