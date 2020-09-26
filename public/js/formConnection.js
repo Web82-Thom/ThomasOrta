0dec90034e209c26d90c7865fa88f731269264f2
Thomas <lefoux82@gmail.com>
2020-09-26 03:44:04 +0200

    create object formConnection

---
+++ public/js/formConnection.js
@@ -0,0 +1,23 @@
+class FormConnection {
+    constructor() {
+        this.eventListenerMethod();
+
+        document.getElementById('email');
+        document.getElementById('password');
+    }
+
+    eventListenerMethod() {
+        this.button = document.getElementById('formButton').addEventListener('click' , () => this.verification());
+    }
+
+    verification() {
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+
+        if (email !== "" && password !== "" ) {
+            this.eventListenerMethod();
+        } else {
+            alert('Veuillez remplir tous les champs correctement.');
+        }
+    }
+}
