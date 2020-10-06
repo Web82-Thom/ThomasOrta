45fc0a0db7f31805632b056b03ffb7df8a953d2f
Thomas <lefoux82@gmail.com>
2020-10-06 17:30:32 +0200

    delete css validator

--- view/footer.php
+++ view/footer.php
@@ -52,10 +52,5 @@
 		<p id="webCreator">
             © 2020 Thomas ORTA
         </p>
-		<p>
-			<a href="http://jigsaw.w3.org/css-validator/check/referer" >
-				<img style = "border: 0; width: 88px; height: 31px" src = "http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Valide !" />
-			</a>
-		</p>
     </section>
 </footer>
